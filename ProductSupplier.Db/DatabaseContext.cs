3df3bc0b20e105e3b5ddbd7e35f58d5d5b6ef7b0
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-12-17 13:58:56 +0300

    Добавил DatabaseContext: DbContext

---
+++ ProductSupplier.Db/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ProductSupplier.Models;
+
+namespace ProductSupplier.Db
+{
+    public class DatabaseContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
