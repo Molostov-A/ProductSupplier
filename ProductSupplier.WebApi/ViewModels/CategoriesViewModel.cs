bed1e6dc7ef913c854d327eea28ca2b210ead9d8
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-12-17 22:48:26 +0300

    Выделил ViewModels в отдельную папку

---
+++ ProductSupplier.WebApi/ViewModels/CategoriesViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductSupplier.WebApi.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
