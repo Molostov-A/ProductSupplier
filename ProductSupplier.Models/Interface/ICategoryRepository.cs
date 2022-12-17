a1aa05af4d8dd7cdc94953ab2caeb7b4656e416c
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-12-17 19:37:48 +0300

    Добавил интерфейс для продуктов и категорий

---
+++ ProductSupplier.Models/Interface/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ProductSupplier.Models.Interface
+{
+    public interface ICategoryRepository
+    {
+        List<Category> GetAll();
+        Category Get(int id);
+        void Add(Category category);
+        void Delete(Category category);
+        void Update(Guid idOldCategory, Category newCategory);
+        Product Find(Guid id);
+    }
+}
---
+++ ProductSupplier.Models/Interface/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductSupplier.Models.Interface
+{
+    public interface IProductRepository
+    {
+        List<Product> GetAll();
+        Product Get(int id);
+        void Add(Product product);
+        void Delete(Product product);
+        void Update(Guid idOldProduct, Product newProduct);
+        Product Find (Guid id);
+    }
+}
