32a837918c48bb1f0b8233a1c52d1a53ec51e55a
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-12-17 14:17:04 +0300

    Изменил контроллер

---
+++ ProductSupplier.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductSupplier.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        //private readonly 
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
