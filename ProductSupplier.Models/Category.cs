7aa628d158a5885cdf3ad9547c4e39695b23ee2a
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-12-18 14:41:00 +0300

    Решил пробему с циклическими ссылками через [JsonIgnore]

--- ProductSupplier.Models/Category.cs
+++ ProductSupplier.Models/Category.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace ProductSupplier.Models
 {
@@ -8,6 +10,8 @@ namespace ProductSupplier.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [JsonIgnore]
         public List<Product> Products { get; set; }
     }
 }
--- ProductSupplier.Models/Product.cs
+++ ProductSupplier.Models/Product.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace ProductSupplier.Models
 {
@@ -9,6 +11,7 @@ namespace ProductSupplier.Models
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
+        
         public List<Category> Categories { get; set; }
     }
 }
